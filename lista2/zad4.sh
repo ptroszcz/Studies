760e191231ec821bf99cfaf5735b27dd6fdf576d
ptroszcz <248925@student.pwr.edu.pl>
2020-10-15 15:03:36 +0200

    Dodano sprawdzanie zmiennych systemowych

--- lista2/zad4.sh
+++ lista2/zad4.sh
@@ -17,6 +17,19 @@ while getopts ":n:m:a:" opt; do
     esac
 done
 
+if [ -n "$MaskName" ]; then
+    Mask=$MaskName
+fi
+
+if [ -n "$LastModifyDays" ]; then
+    Days=$LastModifyDays
+fi
+
+if [ -n "$TarName" ]; then
+    Tar=$TarName
+fi
+
+
 if [ -z "$Days" ]; then
     echo Podaj maksymalna liczbe dni, od ktorych plik byl ostatnio modyfikowany:
     read Days
@@ -27,7 +40,7 @@ if [ -z "$Days" ]; then
 fi
 
 if [ -z "$Mask" ]; then
-    echo Podaj wyszukiwana maske lub rozszerzenie:
+    echo Podaj wyszukiwana maske lub rozszerzenie\(Nie moze znajdowac sie miedzy apostrofami\):
     read Mask
     if [ -z "$Mask" ]; then
 	Mask=$DefaultMask
@@ -43,5 +56,6 @@ if [ -z "$Tar" ]; then
 	echo Nie wybrano wartosci parametru. Ustalo nazwe archiwom na $DefaultTar
     fi
 fi
+
 
 find $HOME -mtime -$Days -name $Mask -print0 | tar -cvf $Tar --null -T -
